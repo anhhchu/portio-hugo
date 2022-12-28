8e444e474ef946a2d84f2a02514549d777066513
Anh Hoang Chu <anhhoangchu@microsoft.com>
2022-12-28 10:41:19 -0800

    Modify layout and slider for testimonial

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,7 +140,7 @@ $(document).ready(function () {
   // Testimonial slider
   $(".testimonial__slider").slick({
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     dots: true,
     arrows: true,
